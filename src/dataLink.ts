34f7d570b5985c2c7caa361f2aaa80a8163f8319
Andrew Hackmann <ajhack93@gmail.com>
2023-01-24 01:55:11 -0600

    queries need to use the regexp

--- src/dataLink.ts
+++ src/dataLink.ts
@@ -1,7 +1,8 @@
-import { DataFrame, DataLink, DataQueryError, DataQueryResponse, isDataFrame } from '@grafana/data';
+import { ArrayVector, DataFrame, DataLink, DataQueryError, DataQueryResponse, Field, FieldType, isDataFrame } from '@grafana/data';
 import { getDataSourceSrv } from '@grafana/runtime';
 import { DataLinkConfig } from 'grafana-plugin-ui';
 import { LogScaleQuery } from 'types';
+import { groupBy } from 'lodash';
 
 export function transformBackendResult(
   response: DataQueryResponse,
@@ -28,14 +29,17 @@ export function transformBackendResult(
 
 function processStreamsFrames(frames: DataFrame[], derivedFieldConfigs: DataLinkConfig[]): DataFrame[] {
   return frames.map((frame) => {
-    return applyLinksToFrame(frame, derivedFieldConfigs);
+    return {
+      ...frame,
+      fields: [...frame.fields, ...getDerivedFields(frame, derivedFieldConfigs)],
+    }
   });
 }
 
 /**
  * Modifies dataframe and adds dataLinks from the config.
  */
-export function applyLinksToFrame(dataFrame: DataFrame, dataLinks: DataLinkConfig[]) {
+export function applyLinksToFrame(dataFrame: DataFrame, dataLinks: DataLinkConfig[]): DataFrame {
   const dataSourceSrv = getDataSourceSrv();
   if (dataLinks.length) {
     for (const dataLink of dataLinks) {
@@ -87,4 +91,75 @@ function improveError(error: DataQueryError | undefined, queryMap: Map<string, L
   }
 
   return error;
+}
+
+function getDerivedFields(dataFrame: DataFrame, derivedFieldConfigs: DataLinkConfig[]): Field[] {
+  if (!derivedFieldConfigs.length) {
+    return [];
+  }
+  const derivedFieldsGrouped = groupBy(derivedFieldConfigs, 'label');
+
+  const newFields = Object.values(derivedFieldsGrouped).map(fieldFromDerivedFieldConfig);
+
+  // line-field is the first string-field
+  // NOTE: we should create some common log-frame-extra-string-field code somewhere
+  const lineField = dataFrame.fields.find((f) => f.type === FieldType.string);
+
+  if (lineField === undefined) {
+    // if this is happening, something went wrong, let's raise an error
+    throw new Error('invalid logs-dataframe, string-field missing');
+  }
+
+  lineField.values.toArray().forEach((line) => {
+    for (const field of newFields) {
+      const logMatch = line.match(derivedFieldsGrouped[field.name][0].matcherRegex);
+      field.values.add(logMatch && logMatch[0]);
+    }
+  });
+
+  return newFields;
+}
+
+/**
+ * Transform derivedField config into dataframe field with config that contains link.
+ */
+function fieldFromDerivedFieldConfig(derivedFieldConfigs: DataLinkConfig[]): Field<any, ArrayVector> {
+  const dataSourceSrv = getDataSourceSrv();
+
+  const dataLinks = derivedFieldConfigs.reduce((acc, derivedFieldConfig) => {
+    // Having field.datasourceUid means it is an internal link.
+    if (derivedFieldConfig.datasourceUid) {
+      const dsSettings = dataSourceSrv.getInstanceSettings(derivedFieldConfig.datasourceUid);
+
+      acc.push({
+        // Will be filled out later
+        title: derivedFieldConfig.field || '',
+        url: '',
+        // This is hardcoded for Jaeger or Zipkin not way right now to specify datasource specific query object
+        internal: {
+          query: { query: derivedFieldConfig.url },
+          datasourceUid: derivedFieldConfig.datasourceUid,
+          datasourceName: dsSettings?.name ?? 'Data source not found',
+        },
+      });
+    } else if (derivedFieldConfig.url) {
+      acc.push({
+        // We do not know what title to give here so we count on presentation layer to create a title from metadata.
+        title: derivedFieldConfig.field || '',
+        // This is hardcoded for Jaeger or Zipkin not way right now to specify datasource specific query object
+        url: derivedFieldConfig.url,
+      });
+    }
+    return acc;
+  }, [] as DataLink[]);
+
+  return {
+    name: derivedFieldConfigs[0].label,
+    type: FieldType.string,
+    config: {
+      links: dataLinks,
+    },
+    // We are adding values later on
+    values: new ArrayVector<string>([]),
+  };
 }
