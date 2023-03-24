fe91bd45d25229992af5c544f04e5e7f071d6908
Andrew Hackmann <ajhack93@gmail.com>
2023-03-24 00:12:10 -0500

    fix bar count issue

--- src/logs.ts
+++ src/logs.ts
@@ -41,9 +41,12 @@ function processFrames(frames: DataFrame[], dataLinkConfigs: DataLinkConfig[], r
 
 function orderFields(fields: Array<Field<any, Vector<any>>>): Array<Field<any, Vector<any>>> {
   const rawstringFieldIndex = fields.findIndex(x => x.name === "@rawstring");
+  if (rawstringFieldIndex === -1) {
+    return fields;
+  }
   const rawstringField = fields.splice(rawstringFieldIndex, 1)[0];
   if (rawstringField) {
-    return [rawstringField, ...fields]
+    return [{ ...rawstringField }, ...fields]
   }
   return fields;
 }
