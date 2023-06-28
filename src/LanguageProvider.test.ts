cfb76e057f973e9161ac6c3c262c70072ae64040
Andrew Hackmann <ajhack93@gmail.com>
2023-06-28 09:34:59 -0500

    rxjs transform

--- src/LanguageProvider.test.ts
+++ src/LanguageProvider.test.ts
@@ -3,22 +3,17 @@ import { AbstractLabelOperator, AbstractQuery, DataSourceInstanceSettings, DataS
 import LanguageProvider from './LanguageProvider';
 import { DataSource } from './DataSource';
 import { LogScaleOptions, LogScaleQuery } from './types';
-import { TemplateSrv } from '@grafana/runtime';
 
 describe('transform abstract query to a LogScale query', () => {
   let datasource: DataSource;
   beforeEach(() => {
-    const templateSrvStub = {
-      getAdhocFilters: jest.fn(() => []),
-      replace: jest.fn((a: string) => a),
-    } as unknown as TemplateSrv;
-
-    datasource = createDataSource({}, templateSrvStub);
+    datasource = createDataSource();
   });
 
   it('with labels and a repo', () => {
     const instance = new LanguageProvider(datasource);
     const abstractQuery: AbstractQuery = {
+      refId: 'bar',
       labelMatchers: [
         { name: '__name__', operator: AbstractLabelOperator.Equal, value: 'repo' },
         { name: 'label1', operator: AbstractLabelOperator.Equal, value: 'value1' },
@@ -32,12 +27,14 @@ describe('transform abstract query to a LogScale query', () => {
     expect(result).toEqual({
       lsql: "label1=\"value1\"\n| label2 != \"value2\"\n| label3 = *value3*\n| label4 != *value4*",
       repository: "repo",
+      refId: abstractQuery.refId,
     } as LogScaleQuery);
   });
 
   it('with a repo', () => {
     const instance = new LanguageProvider(datasource);
     const abstractQuery: AbstractQuery = {
+      refId: 'bar',
       labelMatchers: [
         { name: '__name__', operator: AbstractLabelOperator.Equal, value: 'repo' },
       ],
@@ -47,15 +44,17 @@ describe('transform abstract query to a LogScale query', () => {
     expect(result).toEqual({
       lsql: "",
       repository: "repo",
+      refId: abstractQuery.refId,
     } as LogScaleQuery);
   });
 
   it('with no labels and repo', () => {
     const instance = new LanguageProvider(datasource);
-    const abstractQuery = { labelMatchers: []};
+    const abstractQuery = { labelMatchers: [], refId: 'foo' };
     const result = instance.importFromAbstractQuery(abstractQuery);
 
     expect(result).toEqual({
+      refId: abstractQuery.refId,
       lsql: "",
       repository: "",
     } as LogScaleQuery);
@@ -63,12 +62,9 @@ describe('transform abstract query to a LogScale query', () => {
 });
 
 export function createDataSource(
-  settings: Partial<DataSourceInstanceSettings<LogScaleOptions>> = {},
-  templateSrv: TemplateSrv
 ) {
-  const { jsonData, ...rest } = settings;
 
-  const instanceSettings = {
+  const instanceSettings= {
     id: 1,
     meta: {
       id: 'id',
@@ -83,11 +79,9 @@ export function createDataSource(
     jsonData: {
       authenticateWithToken: false,
       defaultRepository: '',
-      ...jsonData,
     },
     database: '[test-]YYYY.MM.DD',
-    ...rest,
   } as DataSourceInstanceSettings<LogScaleOptions>;
 
-  return new DataSource(instanceSettings, templateSrv);
+  return new DataSource(instanceSettings);
 }
