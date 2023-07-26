2ef98fa318ae213e51a95acffb5441df9c894990
Andrew Hackmann <ajhack93@gmail.com>
2023-07-26 13:25:00 -0500

    wip

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -6,6 +6,7 @@ import {
   DataSourceInstanceSettings,
   DataSourceWithQueryImportSupport,
   MetricFindValue,
+  QueryFixAction,
   ScopedVar,
   vectorator,
 } from '@grafana/data';
@@ -23,7 +24,7 @@ export class DataSource extends DataSourceWithBackend<LogScaleQuery, LogScaleOpt
 
   constructor(
     private instanceSettings: DataSourceInstanceSettings<LogScaleOptions>,
-    readonly templateSrv: TemplateSrv = getTemplateSrv()
+    private readonly templateSrv: TemplateSrv = getTemplateSrv()
   ) {
     super(instanceSettings);
     this.defaultRepository = instanceSettings.jsonData.defaultRepository;
@@ -68,11 +69,110 @@ export class DataSource extends DataSourceWithBackend<LogScaleQuery, LogScaleOpt
   applyTemplateVariables(query: LogScaleQuery, scopedVars: ScopedVar): Record<string, any> {
     return {
       ...query,
-      lsql: getTemplateSrv().replace(query.lsql, scopedVars),
+      lsql: this.templateSrv.replace(query.lsql, scopedVars),
     };
   }
 
   async importFromAbstractQueries(abstractQueries: AbstractQuery[]): Promise<LogScaleQuery[]> {
     return abstractQueries.map((abstractQuery) => this.languageProvider.importFromAbstractQuery(abstractQuery));
+  }
+
+  // By implementing getTagKeys and getTagValues we add ad-hoc filters functionality
+  async getTagKeys() {
+    return await this.labelNamesQuery();
+  }
+
+  async getTagValues(options: any = {}) {
+    return await this.labelValuesQuery(options.key);
+  }
+
+  async labelNamesQuery() {
+    const url = 'labels';
+    const params = this.getTimeRangeParams();
+    const result = await this.metadataRequest(url, params);
+    return result.map((value: string) => ({ text: value }));
+  }
+
+  async labelValuesQuery(label: string) {
+    const params = this.getTimeRangeParams();
+    const url = `label/${label}/values`;
+    const result = await this.metadataRequest(url, params);
+    return result.map((value: string) => ({ text: value }));
+  }
+
+  getTimeRangeParams() {
+    const timeRange = this.getTimeRange();
+    return { start: timeRange.from.valueOf() * NS_IN_MS, end: timeRange.to.valueOf() * NS_IN_MS };
+  }
+
+  async metadataRequest(url: string, params?: Record<string, string | number>) {
+    // url must not start with a `/`, otherwise the AJAX-request
+    // going from the browser will contain `//`, which can cause problems.
+    if (url.startsWith('/')) {
+      throw new Error(`invalid metadata request url: ${url}`);
+    }
+
+    const res = await this.getResource(url, params);
+    return res.data || [];
+  }
+
+  modifyQuery(query: LogScaleQuery, action: QueryFixAction): LogScaleQuery {
+    let expression = query.expr ?? '';
+    switch (action.type) {
+      case 'ADD_FILTER': {
+        if (action.options?.key && action.options?.value) {
+          const value = escapeLabelValueInSelector(action.options.value);
+          expression = addLabelToQuery(expression, action.options.key, '=', value);
+        }
+        break;
+      }
+      case 'ADD_FILTER_OUT': {
+        if (action.options?.key && action.options?.value) {
+          const value = escapeLabelValueInSelector(action.options.value);
+          expression = addLabelToQuery(expression, action.options.key, '!=', value);
+        }
+        break;
+      }
+      case 'ADD_LOGFMT_PARSER': {
+        expression = addParserToQuery(expression, 'logfmt');
+        break;
+      }
+      case 'ADD_JSON_PARSER': {
+        expression = addParserToQuery(expression, 'json');
+        break;
+      }
+      case 'ADD_UNPACK_PARSER': {
+        expression = addParserToQuery(expression, 'unpack');
+        break;
+      }
+      case 'ADD_NO_PIPELINE_ERROR': {
+        expression = addNoPipelineErrorToQuery(expression);
+        break;
+      }
+      case 'ADD_LEVEL_LABEL_FORMAT': {
+        if (action.options?.originalLabel && action.options?.renameTo) {
+          expression = addLabelFormatToQuery(expression, {
+            renameTo: action.options.renameTo,
+            originalLabel: action.options.originalLabel,
+          });
+        }
+        break;
+      }
+      case 'ADD_LABEL_FILTER': {
+        const parserPositions = getParserPositions(query.expr);
+        const labelFilterPositions = getLabelFilterPositions(query.expr);
+        const lastPosition = findLastPosition([...parserPositions, ...labelFilterPositions]);
+        const filter = toLabelFilter('', '', '=');
+        expression = addFilterAsLabelFilter(expression, [lastPosition], filter);
+        break;
+      }
+      case 'ADD_LINE_FILTER': {
+        expression = addLineFilter(expression);
+        break;
+      }
+      default:
+        break;
+    }
+    return { ...query, expr: expression };
   }
 }
