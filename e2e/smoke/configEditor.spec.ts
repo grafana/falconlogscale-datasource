5b5e66c3f86d89237899d7f49c3b36d35cd8268d
Andreas Christou <andreas.christou@grafana.com>
2024-12-13 18:07:46 +0000

    Update smoke tests

--- e2e/smoke/configEditor.spec.ts
+++ e2e/smoke/configEditor.spec.ts
@@ -1,10 +1,12 @@
 import { expect, test } from '@grafana/plugin-e2e';
+import { selectors } from '../../src/e2e/selectors';
 
 test('renders the config editor', async ({ createDataSourceConfigPage, page }) => {
   const configPage = await createDataSourceConfigPage({
     type: 'grafana-falconlogscale-datasource',
   });
 
-  await expect(page.getByText('LogScale Token Authenticatoin')).toBeVisible();
-  await expect(page.getByText('Default Repository')).toBeVisible();
+  await expect(page.getByText('LogScale Token Authentication')).toBeVisible();
+  await expect(page.getByTestId(selectors.components.configEditor.defaultRepository.input)).toBeVisible();
+  await expect(page.getByTestId(selectors.components.configEditor.loadRepositories.button)).toBeVisible();
 });
--- e2e/smoke/query.spec.ts
+++ e2e/smoke/query.spec.ts
@@ -1,10 +1,15 @@
 import { expect, test } from '@grafana/plugin-e2e';
+import { selectors } from '../../src/e2e/selectors';
 
 test.describe('Falcon LogScale queries', () => {
-  test('renders editor', async ({ panelEditPage, page, grafanaVersion }) => {
+  test('renders editor', async ({ panelEditPage }) => {
     await panelEditPage.datasource.set('Falcon LogScale');
 
-    await expect(panelEditPage.getQueryEditorRow('A').getByLabel('Query')).toBeVisible();
-    await expect(panelEditPage.getQueryEditorRow('A').getByLabel('Repository')).toBeVisible();
+    await expect(
+      panelEditPage.getQueryEditorRow('A').getByTestId(selectors.components.queryEditor.queryField.input)
+    ).toBeVisible();
+    await expect(
+      panelEditPage.getQueryEditorRow('A').getByTestId(selectors.components.queryEditor.repository.input)
+    ).toBeVisible();
   });
 });
