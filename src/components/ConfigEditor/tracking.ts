0e545609cbb30708d7bfc54357dfd00e0265b4b9
Andrew Hackmann <5140848+bossinc@users.noreply.github.com>
2026-02-12 11:18:02 -0600

    Update src/components/ConfigEditor/tracking.ts
    Co-authored-by: Adam Yeats <16296989+adamyeats@users.noreply.github.com>

--- src/components/ConfigEditor/tracking.ts
+++ src/components/ConfigEditor/tracking.ts
@@ -1,5 +1,3 @@
 import { reportInteraction } from '@grafana/runtime';
 
-export const trackConfigSetNGSIEMMode = () => {
-    reportInteraction('grafana_falconlogscale_ngsiem_mode_enabled');
-};
+export const trackConfigSetNGSIEMMode = () => reportInteraction('grafana_falconlogscale_ngsiem_mode_enabled');
