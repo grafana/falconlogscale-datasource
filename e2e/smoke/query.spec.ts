a57f79da7921b077fea131b9be8013432bbf3617
Akash Pathak <akash.pathak@grafana.com>
2026-09-08 15:20:04 +0200

    fix(e2e): bump @grafana/plugin-e2e to 3.12.0 to fix the boot-data race in addPanel

--- e2e/smoke/query.spec.ts
+++ e2e/smoke/query.spec.ts
@@ -2,15 +2,15 @@ import { expect, test } from '@grafana/plugin-e2e';
 import { selectors } from '../../src/e2e/selectors';
 
 test.describe('Falcon LogScale queries', () => {
-  test('renders editor', async ({ explorePage, readProvisionedDataSource }) => {
+  test('renders editor', async ({ panelEditPage, readProvisionedDataSource }) => {
     const ds = await readProvisionedDataSource({ fileName: 'logscale.yaml' });
-    await explorePage.datasource.set(ds.name);
+    await panelEditPage.datasource.set(ds.name);
 
     await expect(
-      explorePage.getQueryEditorRow('A').getByTestId(selectors.components.queryEditor.queryField.input)
+      panelEditPage.getQueryEditorRow('A').getByTestId(selectors.components.queryEditor.queryField.input)
     ).toBeVisible();
     await expect(
-      explorePage.getQueryEditorRow('A').getByTestId(selectors.components.queryEditor.repository.input)
+      panelEditPage.getQueryEditorRow('A').getByTestId(selectors.components.queryEditor.repository.input)
     ).toBeVisible();
   });
 });
