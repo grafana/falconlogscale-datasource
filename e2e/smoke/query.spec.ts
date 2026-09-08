bc611679f984c13bd6563b57dcaebbf52722101d
Akash Pathak <akash.pathak@grafana.com>
2026-09-08 14:30:06 +0200

    fix(e2e): move the renders editor smoke test to Explore

--- e2e/smoke/query.spec.ts
+++ e2e/smoke/query.spec.ts
@@ -2,15 +2,15 @@ import { expect, test } from '@grafana/plugin-e2e';
 import { selectors } from '../../src/e2e/selectors';
 
 test.describe('Falcon LogScale queries', () => {
-  test('renders editor', async ({ panelEditPage, readProvisionedDataSource }) => {
+  test('renders editor', async ({ explorePage, readProvisionedDataSource }) => {
     const ds = await readProvisionedDataSource({ fileName: 'logscale.yaml' });
-    await panelEditPage.datasource.set(ds.name);
+    await explorePage.datasource.set(ds.name);
 
     await expect(
-      panelEditPage.getQueryEditorRow('A').getByTestId(selectors.components.queryEditor.queryField.input)
+      explorePage.getQueryEditorRow('A').getByTestId(selectors.components.queryEditor.queryField.input)
     ).toBeVisible();
     await expect(
-      panelEditPage.getQueryEditorRow('A').getByTestId(selectors.components.queryEditor.repository.input)
+      explorePage.getQueryEditorRow('A').getByTestId(selectors.components.queryEditor.repository.input)
     ).toBeVisible();
   });
 });
